5979184b74b9c3f3a73abbd02a7318e9e92961b0
Преподобный Ален <alienufo@inbox.ru>
2019-11-03 17:56:08 +0300

    CString update.

--- include/delphi/Classes.hpp
+++ include/delphi/Classes.hpp
@@ -918,7 +918,7 @@ namespace Delphi {
             size_t find (T Value, size_t Pos = 0) { return Find(Value, Pos); }
             size_t find (LPCTSTR Str, size_t Pos, size_t Length) { return Find(Str, Pos, Length); }
 
-            operator const std::string() const {
+            operator std::string() const {
                 return std::string(this->c_str());
             };
 
